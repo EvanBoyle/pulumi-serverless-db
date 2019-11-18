cab031a407ba9225cafa61941eb180d7916703d0
jmaysrow <maysrowlandjm@mymail.vcu.edu>
2019-11-17 18:52:19 -0800

    add lambda job to create new hourly partitions for data warehouse S3 bucket

--- foo.ts
+++ foo.ts
@@ -1,3 +1,9 @@
+import * as moment from "moment-timezone";
+
 export const foo = () => {
     return "foo"
+}
+
+export const date = () => {
+    return moment("2017-01-01T00:15:48Z").utc().format("YYYY/MM/DD/HH");
 }
